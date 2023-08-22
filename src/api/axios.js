a436dcf3ec77d2baf2c28257ab1a7ca2be7c2645
psalmsin1759 <samson_ude@yahoo.com>
2023-08-22 18:53:51 +0100

    pages updated

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
 export default axios.create({
-  //baseURL: 'https://endpoint.inventroapp.com/api/admin/'
-  baseURL: 'http://localhost:8080/api/admin/'
+  baseURL: 'https://endpoint.inventroapp.com/api/admin/'
+  //baseURL: 'http://localhost:8080/api/admin/'
 })
